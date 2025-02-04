d1a01c3c744d02f56530ddfcf0befb79317cd5ce
F4NTHO <anthony.jean.rene.barret@gmail.com>
2025-02-04 11:33:11 +0100

    le graphique pas beau

--- Script_R/04022025/Seance_1_04022025.R
+++ Script_R/04022025/Seance_1_04022025.R
@@ -82,3 +82,36 @@ ggplot(data=iris, mapping= aes(x=Petal.Length, y=Petal.Width, group=Species))+
   theme_light()
 
 
+########################################################
+########################################################
+######### EXERCICE TD 
+#install.packages("dplyr")
+library(dplyr)
+data("starwars")
+head(starwars)
+
+#Là on dit abscisse espèces et ordonné les masses)
+ggplot(starwars, aes(x = species, y = mass, fill = species)) +
+  #Nos box plots seront noir avec bordure à 0.7
+  geom_boxplot(alpha =0.7, color = "black", outlier.shape =NA)+
+
+  #Là on affiche les points
+  geom_jitter(aes(color= species), width = 0.2, size = 3, alpha=0.7)+
+  #là on mets un titre au graphique)
+  ggtitle("Analyse de la masse des personnages de Star Wars")+
+  #un sous titre
+  labs(subtitle= "Comparaison par espèce(sans les extrêmes)",
+  #On créer des espaces 
+  x = "Espèce", y = "Masse (kg)", fill = "Espèce")+
+  theme_minimal(base_size = 14)+
+  theme(axis.text.x = element_text(angle=45, hjust =1, face="bold",color="white"),
+  axis.text.y = element_text(face="bold", color= "white"),
+  plot.background = element_rect(fill = "black"),
+  panel.background = element_rect(fill="black"),
+  legend.background = element_rect(fill="gray20"),
+  legend.text = element_text(color = "white"),
+  legend.title = element_text(face = "bold", color= "white"),
+  plot.title = element_text(face = "bold", size = 20, color = "gold"),
+  plot.subtitle = element_text(face = "italic", size = 14, color = "gold"))
+  
+
